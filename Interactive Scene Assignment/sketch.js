8e098b9612ab3832a0b8076ae3f482804e80e421
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-02 11:01:44 -0600

    Condensed more code and decided what to do for extra for experts

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -1,15 +1,15 @@
 // Interactive Scene
 // Mehreeb Shahzad
 // Tuesday, March 3, 2026
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Extra for Experts: Hovering text
+// - When the mouse hovers over the different colours, the name of the colour appears above it.
 
 
 let strokeSize = 10;
 let colour = "black";
 let bgColour = "white";
 let colours1 = ["red", "orange", "yellow", "green", "blue", "purple", "black", "gray"];
-let colours2 = ["red", "orange", "yellow", "green", "blue", "purple", "white", "gray"];
+let colours2 = structuredClone(colours1);
 let pixelEraser;
 
 function preload() {
@@ -20,6 +20,9 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("white");
+
+  //Replace the black colour with white
+  colours2[6] = "white";
 }
 
 function draw() {
@@ -30,6 +33,7 @@ function draw() {
 }
 
 function drawing() {
+  //Draws a line from previous mouse location to current mouse location
   if (mouseIsPressed) {
     strokeWeight(strokeSize);
     stroke(colour);
@@ -39,7 +43,7 @@ function drawing() {
 
 function display() {
   let x = 10;
-  let word = ["Background:", "Line Colour:", "Thickness:", "r - resets the canvas"];
+  let word = ["Background:", "Line Colour:", "Stroke Size:", "r - resets the canvas"];
   
   //Menu display
   fill ("white");
@@ -55,7 +59,7 @@ function display() {
     x += 190;
   }
   
-  //Stroke size box
+  //Stroke size boxes
   fill(225);
   square(400, height - 95, 50);
 
@@ -81,16 +85,18 @@ function backgroundDisplay() {
   let x = 25;
   
   strokeWeight(1);
-  for (let i = 0; i <= 3; i++) {
-    fill(colours1[i]);
-    circle(x, height - 95, 30);
-    x += 35;
-  }
-  
-  x = 25;
-  for (let i = 4; i <= 7; i++) {
-    fill(colours1[i]);
-    circle(x, height - 55, 30);
+  for (let i = 0; i < colours1.length; i++) {
+    //Top
+    if (i <= 3) {
+      fill(colours1[i]);
+      circle(x, height - 95, 30);
+    }
+
+    //Bottom
+    if (i > 3) {
+      fill(colours1[i]);
+      circle(x - 140, height - 55, 30);
+    }
     x += 35;
   }
 }
@@ -99,21 +105,24 @@ function inkColourDisplay() {
   let x = 215;
   
   strokeWeight(1);
-  for (let i = 0; i <= 3; i++) {
-    fill(colours2[i]);
-    circle(x, height - 95, 30);
-    x += 35;
-  }
+  for (let i = 0; i < colours2.length; i++) {
+    //Top
+    if (i <= 3) {
+      fill(colours2[i]);
+      circle(x, height - 95, 30);
+    }
 
-  x = 215;
-  for (let i = 4; i <= 7; i++) {
-    fill(colours2[i]);
-    circle(x, height - 55, 30);
+    //Bottom
+    if (i > 3) {
+      fill(colours2[i]);
+      circle(x - 140, height - 55, 30);
+    }
     x += 35;
   }
 }
 
 function keyPressed() {
+  //Resets the whole canvas
   if (key === "r") {
     clear();
     bgColour = "white";
@@ -128,11 +137,13 @@ function mouseClicked() {
 
   //Background colours
   for (let i = 0; i <= 7; i++) {
+    //Top
     if (mouseX >= x && mouseX <= x + 30 && mouseY >= height - 110 && mouseY <= height - 80 && i <= 3) {
       bgColour = colours1[i];
       background(bgColour);
     }
 
+    //Bottom
     if (mouseX >= x - 140 && mouseX <= x - 110 && mouseY >= height - 70 && mouseY <= height - 40 && i > 3) {
       bgColour = colours1[i];
       background(bgColour);
@@ -143,27 +154,30 @@ function mouseClicked() {
   //Pen colours
   x = 200;
   for (let i = 0; i <= 7; i++) {
+    //Top
     if (mouseX >= x && mouseX <= x + 30 && mouseY >= height - 110 && mouseY <= height - 80 && i <= 3) {
       colour = colours2[i];
     }
-
+    
+    //Bottom
     if (mouseX >= x - 140 && mouseX <= x - 110 && mouseY >= height - 70 && mouseY <= height - 40 && i > 3) {
       colour = colours2[i];
     }
     x += 35;
   }
   
-  //Stroke Size
+  //Increase stroke size
   if (mouseX >= 450 && mouseX <= 500 && mouseY >= height - 95 && mouseY <= height - 70) {
     strokeSize++;
   }
   
+  //Decrease stroke size
   if (mouseX >= 450 && mouseX <= 500 && mouseY >= height - 70 && mouseY <= height - 45 && strokeSize > 1) {
     strokeSize--;
   }
 
   //Eraser
-  //Note: to change the size of the eraser, you need to change the thickness
+  //Note: to change the size of the eraser, you need to change the stroke size
   if (mouseX >= 580 && mouseX <= 580 + pixelEraser.width * 0.4 && mouseY >= 840 && mouseY <= 840 + pixelEraser.height * 0.4) {
     colour = bgColour;
   }

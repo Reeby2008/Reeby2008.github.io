101d7f6484fe01daee198ab529d4a3106c7ba0f2
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-04 11:03:13 -0600

    ball moving

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,6 +4,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
@@ -15,21 +16,41 @@ function draw() {
     ball.y += ball.dy;
 
     //Display
+    if (ball.x - ball.radius > width) {
+      ball.x = width + ball.radius;
+    }
+
+    if (ball.x  + ball.radius < width) {
+      ball.x = -width - ball.radius;
+    }
+
+    if (ball.y - ball.radius > height) {
+      ball.y = height + ball.radius;
+    }
+
+    if (ball.y + ball.radius < height) {
+      ball.y = -height - ball.radius;
+    }
+
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius * 2);
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let someBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 30),
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255),
   };
   ballArray.push(someBall);
 }

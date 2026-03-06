9143d8966265f0e5009a1f875bd62209b4eb61fc
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-06 11:02:44 -0600

    Created half-ish of the home screen of the game.

--- arrays-assignment/sketch.js
+++ arrays-assignment/sketch.js
@@ -5,11 +5,34 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let cards = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  homeScreen();
+}
+
+function homeScreen() {
+  let rectSize = {
+    x: width/2 - 50,
+    y: height/2 - 100,
+    w: 100,
+    h: 200,
+  };
+
+  background("black");
+  fill("white");
+  textSize(50);
+  text("SOLITAIRE", rectSize.x, rectSize.y);
+  textSize(30);
+  text("Play", rectSize.x + 50, rectSize.y + 50);
+}
+
+function playScreen() {
+  let cardSize = 50;
+
+  rect(rectSize.x, rectSize.y, rectSize.w, rectSize.h);
 }

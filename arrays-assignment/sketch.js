d2ff547e57f1545eb3995e26dd28e97e366d3c53
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-09 11:01:15 -0600

    Made the default screen look a little better. Need to center the text on the canvas.

--- arrays-assignment/sketch.js
+++ arrays-assignment/sketch.js
@@ -17,22 +17,27 @@ function draw() {
 
 function homeScreen() {
   let rectSize = {
-    x: width/2 - 50,
-    y: height/2 - 100,
-    w: 100,
-    h: 200,
+    x: width/2,
+    y: height/2,
+    w: 700,
+    h: 700,
+    r: 10,
   };
 
-  background("black");
-  fill("white");
+  background(0, 50, 0);
+  fill(250, 250, 150);
+  stroke(100, 150, 100);
+  strokeWeight(10);
+  rect(rectSize.x - rectSize.w/2, rectSize.y - rectSize.h/2, rectSize.w, rectSize.h, rectSize.r, rectSize.r, rectSize.r, rectSize.r);
   textSize(50);
   text("SOLITAIRE", rectSize.x, rectSize.y);
-  textSize(30);
-  text("Play", rectSize.x + 50, rectSize.y + 50);
+  // textSize(30);
+  // text("Play", rectSize.x + 100, rectSize.y + 50);
 }
 
 function playScreen() {
   let cardSize = 50;
 
+  background("green");
   rect(rectSize.x, rectSize.y, rectSize.w, rectSize.h);
 }

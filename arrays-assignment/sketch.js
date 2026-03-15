38a95595bc50bd0ee3e92a3c82262bd4e2c19d33
Reeb <mahreebshahzad@gmail.com>
2026-03-15 15:07:43 -0600

    Uploaded image of a deck of cards. Figured out how to access a cropped version of the same image through the code. Tried to push those images into an array (unsuccessful).

--- arrays-assignment/sketch.js
+++ arrays-assignment/sketch.js
@@ -1,11 +1,17 @@
 // Array/Object Notation Assignment
 // Mehreeb Shahzad
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Wednesday, March 18, 2026
 
-let cards = [];
+// Extra for Experts: Accessing specific parts of an image
+// - Using image() to access specific parts of one image (like one specific card)
+
+let deckOfCards;
+let card = [];
+
+function preload() {
+  //Image source: https://stock.adobe.com/ca/images/playing-cards-full-deck-set-with-isolated-cards/559593180
+  deckOfCards = loadImage("deck-of-cards.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -49,12 +55,25 @@ function playScreen() {
     y: height/4,
   };
 
+  let destx = 100;
+  let desty = 100;
+  let x = 15;
+  let y = 10;
+
   background(0, 50, 0);
   fill("white");
   rect(cardPlaces.x + 75, cardPlaces.y, 150, 200);
   rect(cardPlaces.x + 200, cardPlaces.y, 150, 200);
   rect(cardPlaces.x + 75, cardPlaces.y, 150, 200);
   rect(cardPlaces.x + 75, cardPlaces.y, 150, 200);
+
+  //image(img, destination x, destination y, destination width, destination height, crop x, crop y, crop width, crop height)
+  for (let cards = 0; cards <= 14; cards++) {
+    card.push(deckOfCards.get(destx, desty, x, y));
+    destx += 100;
+    x += 69.5;
+  }
+  console.log(card);
 }
 
 function mouseClicked() {
@@ -66,7 +85,7 @@ function mouseClicked() {
     r: 10,
   };
   
-  if (mouseX <= size.x + textWidth("Play") && mouseX >= size.x - textWidth("Play") && mouseY <= size.y + 75 && mouseY >= size.y + 10) {
+  if (mouseX <= size.x + textWidth("Play")/2 && mouseX >= size.x - textWidth("Play")/2 && mouseY <= size.y + 75 && mouseY >= size.y + 10) {
     playScreen();
   }
 }

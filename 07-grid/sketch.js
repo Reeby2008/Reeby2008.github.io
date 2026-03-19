6a51acd49bee9c792f150e864ebe5cb59fc69d9d
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-19 10:25:15 -0600

    Finished grid demo

--- 07-grid/sketch.js
+++ 07-grid/sketch.js
@@ -1,12 +1,15 @@
 // Grid Demo
 // Learning 3d Arrays
 
-let grid = [[0, 0, 1, 0],
-            [1, 0, 1, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 1]];
-const SQUARE_DIMENSIONS = grid.length;
+// let grid = [[0, 0, 1, 0],
+//             [1, 0, 1, 0],
+//             [0, 1, 0, 0],
+//             [0, 1, 0, 1]];
+// const SQUARE_DIMENSIONS = grid.length;
 
+//Use this to randomize the grid
+let grid;
+const SQUARE_DIMENSIONS = 10;
 let cellSize;
 
 function setup() {
@@ -17,6 +20,7 @@ function setup() {
   else {
     cellSize = height/SQUARE_DIMENSIONS;
   }
+  grid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
 }
 
 function draw() {
@@ -36,4 +40,38 @@ function showGrid() {
       square(x * cellSize, y * cellSize, 50);
     }
   }
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  if (grid[y][x] === 1) {
+    grid[y][x] = 0;
+  }
+  else {
+    grid[y][x] = 1;
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  
+  for (let y = 0; y < rows: y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(0);
+      }
+      else {
+        newGrid[y].push(1);
+      }
+    }
+  }
+
+  return newGrid;
 }

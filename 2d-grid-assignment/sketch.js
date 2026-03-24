9d14f066c00028c977676945c56d30ca13f9b195
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-24 11:03:06 -0600

    Improved game of life demo and started on 2d arrays assignment

--- 2d-grid-assignment/sketch.js
+++ 2d-grid-assignment/sketch.js
@@ -5,11 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let colours = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+let grid = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  gameDisplay();
 }
 
 function draw() {
-  background(220);
+}
+
+function gameDisplay() {
+  //rectMode(CENTER);
+  for (let x = 0; x <= 600; x += 120) {
+    for (let y = 0; y <= 700; y += 120) {
+      //fill(colours[int(random(0, 6))]);
+      rect(x, y, 120, 120);
+    }
+    //grid.push([])
+  }
+  //rect(width/2, height/2, 600, 700);
 }

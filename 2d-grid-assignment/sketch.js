26b8963ac2b91a6cf3223b42ad706c5e8363e515
Reeby2008 <mahreebshahzad@gmail.com>
2026-03-27 11:03:24 -0600

    Boxes change colour when clicked on and store it with the x and y values of that box.

--- 2d-grid-assignment/sketch.js
+++ 2d-grid-assignment/sketch.js
@@ -9,6 +9,8 @@ const SQUARE_SIZE = 120;
 let colours = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black"];
 let grid = [];
 let answer = [];
+let input = [];
+let nextColour = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,15 +23,29 @@ function draw() {
 }
 
 function mousePressed() {
-  for (index = 0; index <= 4; index++) {
-    fill(colours[0]);
-    square(grid[index][0], grid[index][1], SQUARE_SIZE);
+  for (let index = 0; index < 4; index++) {
+    if (nextColour === colours.length) {
+      nextColour = 0;
+    }
+
+    if (mouseX >= grid[index][0] && mouseX <= grid[index][0] + SQUARE_SIZE && mouseY >= grid[index][1] && mouseY <= grid[index][1] + SQUARE_SIZE) {
+      fill(colours[nextColour]);
+      square(grid[index][0], grid[index][1], SQUARE_SIZE);
+      grid[index].push(colours[nextColour]);
+      nextColour++;
+    }
   }
 }
 
+function keyPressed() {
+  // if (key === "ENTER") {
+  //   input.push()
+  // }
+}
+
 function gameDisplay() {
-  for (let x = width/2 - SQUARE_SIZE * 2; x < width/2 + SQUARE_SIZE * 2; x += SQUARE_SIZE) {
-    for (let y = height/2 - SQUARE_SIZE * 2.5; y < height/2 + SQUARE_SIZE * 2.5; y += SQUARE_SIZE) {
+  for (let y = height/2 - SQUARE_SIZE * 2.5; y < height/2 + SQUARE_SIZE * 2.5; y += SQUARE_SIZE) {
+    for (let x = width/2 - SQUARE_SIZE * 2; x < width/2 + SQUARE_SIZE * 2; x += SQUARE_SIZE) {
       square(x, y, SQUARE_SIZE);
       grid.push([x, y]);
     }

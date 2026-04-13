cce575f83c8f31c3afaee10da21c38f15ac1b150
Reeb <mahreebshahzad@gmail.com>
2026-04-12 23:29:29 -0600

    Fixed the issue with errors at the end of the game. Completed the colour detection system (right colour/right place). Polished code.

--- 2d-grid-assignment/sketch.js
+++ 2d-grid-assignment/sketch.js
@@ -1,6 +1,6 @@
 // 2d Grids Assignment
 // Mehreeb Shahzad
-// Date
+// Wednesday, April 15, 2026
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -14,7 +14,6 @@ let userInput = [];
 let temporaryArray = [];
 let nextColour = 0;
 let previousRow = 0;
-//let correctColour = "black";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -29,13 +28,12 @@ function draw() {
 function mousePressed() {
   //If a box is clicked on, change the colour
   //Note: the nextColour variable changes no matter what box is clicked so if a second box is clicked, it will fill with the next colour in the array, not the first colour
-  //Note: at the end of the game the code starts throwing errors when any box is clicked
   for (let index = previousRow; index < NEXT_ROW + previousRow; index++) {
     if (nextColour === colours.length) {
       nextColour = 0;
     }
 
-    if (mouseX >= grid[index][0] && mouseX <= grid[index][0] + SQUARE_SIZE && mouseY >= grid[index][1] && mouseY <= grid[index][1] + SQUARE_SIZE) {
+    if (index < grid.length && mouseX >= grid[index][0] && mouseX <= grid[index][0] + SQUARE_SIZE && mouseY >= grid[index][1] && mouseY <= grid[index][1] + SQUARE_SIZE) {
       fill(colours[nextColour]);
       square(grid[index][0], grid[index][1], SQUARE_SIZE);
       grid[index].push(colours[nextColour]);
@@ -50,7 +48,7 @@ function keyPressed() {
     //Remove previous guess from the array
     userInput.splice(0, 4);
 
-    //Pushes the colours that were entered by the user
+    //Pushes the colours that were entered by the user into the userInput array
     for (let i = previousRow; i < NEXT_ROW + previousRow; i++) {
       userInput.push(grid[i][grid[i].length - 1]);
     }
@@ -64,19 +62,11 @@ function gameDisplay() {
   //Pushes the x and y values of each square into the grid array and displays the grid
   for (let y = height/2 - SQUARE_SIZE * 2.5; y < height/2 + SQUARE_SIZE * 2.5; y += SQUARE_SIZE) {
     for (let x = width/2 - SQUARE_SIZE * 2; x < width/2 + SQUARE_SIZE * 2; x += SQUARE_SIZE) {
-      //stroke(correctColour);
       strokeWeight(5);
       square(x, y, SQUARE_SIZE);
-      grid.push([x, y]);
+      grid.push([x, y, "white"]);
     }
   }
-
-  //Displays the answer bar
-  // for (let y = height/2 - SQUARE_SIZE * 3.75; y < height/2 - SQUARE_SIZE * 2.75; y += SQUARE_SIZE) {
-  //   for (let x = width/2 - SQUARE_SIZE * 2; x < width/2 + SQUARE_SIZE * 2; x += SQUARE_SIZE) {
-  //     square(x, y, SQUARE_SIZE);
-  //   }
-  // }
 }
 
 function gameAnswer() {
@@ -91,44 +81,24 @@ function gameAnswer() {
 function colourPlacement() {
   temporaryArray.splice(0, NEXT_ROW);
 
-  for (let sequence = 0; sequence < answer.length; sequence++) {
-    //If the colours are in the right spot
+  for (let sequence = 0; sequence < userInput.length; sequence++) {
 
+    //If the colours are in the right spot
     if (userInput[sequence] === answer[sequence]) {
-      //correctColour = "green";
       temporaryArray.push(true);
     }
 
-    else {
-      //correctColour = "black";
-      //If the colours are correct, but in the wrong spot
-      for (let correctColour = 0; correctColour < answer.length; correctColour++) {
-        if (userInput[sequence] === answer[correctColour] && sequence !== correctColour) {
-          temporaryArray.push("right");
-        }
-  
-        // else {
-        //   temporaryArray.push(false);
-        // }
-      }
-
-      // if (userInput[sequence] === answer[0] && sequence !== 0) {
-      //   temporaryArray.push("right");
-      // }
-
-      // if (userInput[sequence] === answer[1] && sequence !== 1) {
-      //   temporaryArray.push("right");
-      // }
-
-      // if (userInput[sequence] === answer[2] && sequence !== 2) {
-      //   temporaryArray.push("right");
-      // }
-
-      // if (userInput[sequence] === answer[3] && sequence !== 3) {
-      //   temporaryArray.push("right");
-      // }
+    //If the colours are correct, but in the wrong spot
+    else if (userInput[sequence] === answer[0] ||
+             userInput[sequence] === answer[1] ||
+             userInput[sequence] === answer[2] ||
+             userInput[sequence] === answer[3]) {
+      temporaryArray.push("correct colour");
     }
-    temporaryArray.push(false);
+
+    else {
+      temporaryArray.push(false);
+    }
   }
 
   console.log(temporaryArray);

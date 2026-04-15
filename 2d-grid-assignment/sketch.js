7a75dce212506f21139427a7f8122c720ff05b8c
Reeb <mahreebshahzad@gmail.com>
2026-04-14 21:08:41 -0600

    Finished winning and losing end screens. Made it so that colours don't repeat.

--- 2d-grid-assignment/sketch.js
+++ 2d-grid-assignment/sketch.js
@@ -1,13 +1,13 @@
-// 2d Grids Assignment
+// 2d Grids Assignment (Inspired by Mastermind from assignment exemplars)
 // Mehreeb Shahzad
 // Wednesday, April 15, 2026
 //
-// Extra for Experts:
+// Extra for Experts: Background Music
 // - describe what you did to take this project "above and beyond"
 
 const SQUARE_SIZE = 120;
 const SQUARES_GAP = 10;
-const NEXT_ROW = 4;
+const ROW_SIZE = 4;
 let colours = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "white"];
 let grid = [];
 let answer = [];
@@ -26,7 +26,7 @@ function mousePressed() {
   //If a box is clicked on, change the colour
   //Note: the nextColour variable changes no matter what box is clicked so if a second box is clicked, it will fill with the next colour in the array, not the first colour
   //Note: even after the sequence of colours is guessed correctly, the next row can still be clicked on and changed
-  for (let index = previousRow; index < NEXT_ROW + previousRow; index++) {
+  for (let index = previousRow; index < ROW_SIZE + previousRow; index++) {
     //If user reaches last colour in the array, loop back to first colour
     if (nextColour === colours.length) {
       nextColour = 0;
@@ -49,10 +49,10 @@ function keyPressed() {
   //Locks in previous answer and pushes to a different array
   if (keyCode === ENTER) {
     //Remove previous guess from the array
-    userInput.splice(0, 4);
+    userInput.splice(0, ROW_SIZE);
     
     //Pushes the colours that were entered by the user into the userInput array
-    for (let i = previousRow; i < NEXT_ROW + previousRow; i++) {
+    for (let i = previousRow; i < ROW_SIZE + previousRow; i++) {
       userInput.push(grid[i][grid[i].length - 1]);
     }
 
@@ -66,8 +66,8 @@ function keyPressed() {
 
 function gameDisplay() {
   //Pushes the x and y values of each square into the grid array and displays the grid
-  for (let y = height/2 - SQUARE_SIZE * 2.5; y < height/2 + SQUARE_SIZE * 2.5 + SQUARES_GAP * 4; y += SQUARE_SIZE) {
-    for (let x = width/2 - SQUARE_SIZE * 2; x < width/2 + SQUARE_SIZE * 2 + SQUARES_GAP * 4; x += SQUARE_SIZE) {
+  for (let y = height/2 - SQUARE_SIZE * 2.5; y < height/2 + SQUARE_SIZE * 2.5 + SQUARES_GAP * 3; y += SQUARE_SIZE) {
+    for (let x = width/2 - SQUARE_SIZE * 2; x < width/2 + SQUARE_SIZE * 2 + SQUARES_GAP * 3; x += SQUARE_SIZE) {
       strokeWeight(5);
       square(x, y, SQUARE_SIZE);
       grid.push([x, y, "white"]);
@@ -79,10 +79,15 @@ function gameDisplay() {
 
 function gameAnswer() {
   //Picks random colours to be the answer to the game
-  for (let answers = 0; answers < 4; answers++) {
-    let randomColour = colours[int(random(0, colours.length))];
+  let noRepeat = structuredClone(colours);
+  
+  for (let answers = 0; answers < ROW_SIZE; answers++) {
+    let randomIndex = int(random(0, noRepeat.length));
     
-    answer.push(randomColour);
+    answer.push(noRepeat[randomIndex]);
+
+    //No repeating colours
+    noRepeat.splice(randomIndex, 1);
   }
 }
 
@@ -112,12 +117,21 @@ function colourPlacement() {
 
 function endScreen() {
   //If user wins
-  if (userInput === answer) {
-    text("You Win!", width/2, height/2);
+  if (userInput[0] === answer[0] &&
+      userInput[1] === answer[1] &&
+      userInput[2] === answer[2] &&
+      userInput[3] === answer[3]) {
+    strokeWeight(0);
+    textSize(25);
+    fill("black");
+    text("You Win!", width/2, (height/2 - SQUARE_SIZE * 2.5)/2);
   }
 
   //If user loses
-  if (previousRow === 16 && userInput !== answer) {
-    text("You Lose", width/2, height/2);
+  else if (previousRow === 16) {
+    strokeWeight(0);
+    textSize(25);
+    fill("black");
+    text("You Lose", width/2, height/2 - SQUARE_SIZE * 2.5);
   }
 }
